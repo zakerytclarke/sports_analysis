1d5bf581ef762de53b921fd4ddddca632376b468
Zakery Clarke <zakerytclarke@gmail.com>
2022-12-11 16:36:12 -0500

    Added team colors

--- main.py
+++ main.py
@@ -4,47 +4,59 @@ from PIL import Image, ImageDraw
 import cv2
 import math 
 import torch
+import numpy as np
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-
+model.conf = 0.01
 
 execution_path = os.getcwd()
 
 os.system("rm -rf output && mkdir output")
 os.system("rm -rf runs")
 
-# detector = ObjectDetection()
-# detector.setModelTypeAsRetinaNet()
-# detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
-# detector.loadModel()
-# custom_objects = detector.CustomObjects(car=True, motorcycle=True)
-SAMPLE_RATE = 2
+SAMPLE_RATE = 1
 
 MAX_DISTANCE = 30
 
-def getDetections(raw,w,h):
+
+def getDetections(raw, configs):
   out = []
-  names = raw.names
-  
-  for tensor in raw.xyxyn[0]:
-    box=tensor.tolist()[0:4]
-    out.append({
-      'name':names[tensor.tolist()[5]],
-      'box_points':[w*box[0],h*box[1],w*box[2],h*box[3]]
-    })
+  names=raw.names
+
+  df = raw.pandas().xyxy[0]
+
+  df['objects'] = df.apply(lambda row: {
+        'name':names[row['class']],
+        'box_points':[row.xmin,row.ymin,row.xmax,row.ymax],
+        'confidence':row.confidence
+      },axis=1)
+  for obj in df['objects']:
+    if obj.get('name') in configs and obj.get('confidence') >= configs[obj.get('name')].get('confidence'):
+      out.append(obj)
 
   return out
 
 class Object:
-  def __init__(self, label, cx, cy):
+  def __init__(self, label, cx, cy, box_points, config):
+    # Position
     self.xs = [cx]
     self.ys = [cy]
+
+    self.box_points = [box_points]
+    # Velocity
     self.vxs = [0]
     self.vys = [0]
+    # Offset
     self.oxs = [0]
     self.oys = [0]
+    # Cumulative offset
+    self.coxs = [0]
+    self.coys = [0]
+
 
     self.label = label
+    self.disabled = False # Whether object is in frame 
     
+    self.config = config
 
   def update_nearest(self, detections):
     best={
@@ -52,6 +64,7 @@ class Object:
       'x':0,
       'y':0
     }
+    # Search through objects with same label
     for detection in filter(lambda x:x.get('name')==self.label, detections):
       cx=(detection.get('box_points')[0]+detection.get('box_points')[2])/2
       cy=(detection.get('box_points')[1]+detection.get('box_points')[3])/2
@@ -63,27 +76,32 @@ class Object:
           'distance':distance,
           'x':cx,
           'y':cy,
+          'box_points':detection.get('box_points'),
           'obj':detection
         }
     
 
     # Determine if same object
-    if best.get('distance') <= MAX_DISTANCE:
+    if best.get('distance') <= self.config.get('max_distance'):
       # Update locations
       self.xs.append(best.get('x'))
       self.ys.append(best.get('y'))
+      self.box_points.append(best.get('box_points'))
       # Update velocities 
       self.vxs.append(self.xs[-1]-self.xs[-2])
       self.vys.append(self.xs[-1]-self.xs[-2]) 
       # Flag as belonging to another object
       best.get('obj')['processed'] = True
-
+    else:
+      self.disabled = True
 
     
 
 class ObjectTracker:
-  def __init__(self, video):
-    self.video = video
+  def __init__(self, config):
+    self.video = config.get('video')
+    assert self.video, "Please provide a video url"
+    self.object_settings = config.get('objects')
     # Detection objects we want to track
     self.objects = []
 
@@ -92,7 +110,8 @@ class ObjectTracker:
       for detection in detections:
         cx=(detection.get('box_points')[0]+detection.get('box_points')[2])/2
         cy=(detection.get('box_points')[1]+detection.get('box_points')[3])/2
-        self.objects.append(Object(detections.get('name'),cx,cy))
+        if detection.get('name') in self.object_settings:
+          self.objects.append(Object(detection.get('name'),cx,cy,detection.get('box_points'),self.object_settings[detection.get('name')]))
     else:
       for obj in self.objects:
         # Update object locations
@@ -107,12 +126,15 @@ class ObjectTracker:
         obj.ys[-1] += avg_offset_y
         obj.oxs.append(avg_offset_x)
         obj.oys.append(avg_offset_y)
+        obj.coxs.append(obj.coxs[-1]+obj.oxs[-1])
+        obj.coys.append(obj.coys[-1]+obj.oys[-1])
       # Find unlabeled objects
       new_objs = list(filter(lambda x:not x.get('processed',False),detections))
       for n_obj in new_objs:
         cx=(n_obj.get('box_points')[0]+n_obj.get('box_points')[2])/2
         cy=(n_obj.get('box_points')[1]+n_obj.get('box_points')[3])/2
-        self.objects.append(Object(n_obj.get('name'),cx,cy))
+        if n_obj.get('name') in self.object_settings:
+          self.objects.append(Object(n_obj.get('name'),cx,cy,n_obj.get('box_points'),self.object_settings[n_obj.get('name')]))
         
 
 
@@ -124,21 +146,18 @@ class ObjectTracker:
 
     while success:
       success,image = vidcap.read()
-      h, w, c = image.shape
-      if count % SAMPLE_RATE == 0:
+      
+      if success and count % SAMPLE_RATE == 0:
         print(count)
       
-        image_path = f"./output/frame{frame_count}"
+        image_path = f"./output/frame{str(frame_count).zfill(5)}"
         cv2.imwrite(image_path+".jpg", image)     # save frame as JPEG file      
         
-
-        # detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , image_path+".jpg"), output_image_path=os.path.join(execution_path , image_path+"_detected.jpg"), custom_objects = detector.CustomObjects(person=True, sports_ball=True), minimum_percentage_probability=15)
-        
-        
         results = model(image)
-        results.save(image_path+"_detected.jpg")
         
-        detections = getDetections(results,w,h)
+        # results.save(image_path+"_detected.jpg")
+        
+        detections = getDetections(results, self.object_settings)
         
         self.update_objects(detections)
         # Annotate image
@@ -147,30 +166,59 @@ class ObjectTracker:
         for object in self.objects:
           ox = 0
           oy = 0
-          import ipdb
-          ipdb.set_trace()
-          draw.ellipse((object.xs[-1]-20, object.ys[-1]-10+20, object.xs[-1]+20, object.ys[-1]+10+20), width=5, outline='#2bd68c')
-        image.save(image_path+"_annotated.jpg")
+          
+
+          if not object.disabled:
+            obj_settings = self.object_settings[object.label]
+            if obj_settings:
+              if obj_settings.get('color')=='average':
+                b,g,r = list(np.average(np.average(image.crop(object.box_points[-1]), axis=0), axis=0))
+                color = (int(r),int(g),int(b))
+              else:
+                color = obj_settings.get('color')
+                
+              marker = obj_settings.get('marker')
+              # Draw marker
+              if marker=='ellipse':
+                draw.ellipse((object.xs[-1]-20, object.ys[-1]-10+20, object.xs[-1]+20, object.ys[-1]+10+20), width=5, outline=color)
+              elif marker=='circle':
+                draw.ellipse((object.xs[-1]-10, object.ys[-1]-10, object.xs[-1]+10, object.ys[-1]+10), width=5, outline=color)
+              elif marker=='box':
+                draw.rounded_rectangle(object.box_points[-1], width=3, radius=5, outline=color)
+              
+              # Draw Velocity
+              if 'show_velocity' in obj_settings:
+                draw.line((object.xs[-1], object.ys[-1], object.xs[-1] + object.vxs[-1] - object.oxs[-1], object.ys[-1] + object.vys[-1] - object.oys[-1]), width=2, fill=color)
+
+        image.save(image_path+".jpg")
         frame_count += 1
       count += 1
+    print("Finished Processing")
+    os.system("ffmpeg -r 40 -f image2 -i ./output/frame%05d.jpg -vcodec libx264 -crf 25  -pix_fmt yuv420p processed.mp4 -y")
+    
 
 
 
 
 
-sports_game = ObjectTracker("./soccer.mp4")
+sports_game = ObjectTracker({
+  'video':"./soccer.mp4",
+  'objects':{
+    'sports ball':{
+      'confidence':0.1,
+      'max_distance':50,
+      'color':'white',
+      'marker':'circle',
+      'show_velocity':True
+    },
+    'person':{
+      'confidence':0.3,
+      'max_distance':30,
+      # 'color':'#2bd68c',
+      'color':'average',
+      'marker':'ellipse',
+      'show_velocity':True
+    }
+  }
+})
 sports_game.process()
-
-
-
-
-
-# def line(image_path, output_path):
-#     image = Image.open(image_path)
-#     draw = ImageDraw.Draw(image)
-#     colors = ["red", "green", "blue", "yellow",
-#               "purple", "orange"]
-#     for i in range(0, 100, 20):
-#         draw.line((i, 0) + image.size, width=5, 
-#                   fill=random.choice(colors))
-#     image.show()
